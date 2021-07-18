fcc07c04e0b9d81f7960191fd3209d5a67def581
William Ennals <William.Ennals@cvshealth.com>
2021-07-18 15:36:08 -0400

    Dynamic file naming

--- src/artistPairFinder.ts
+++ src/artistPairFinder.ts
@@ -9,7 +9,8 @@ export interface ArtistMap {
 
 export class ArtistPairFinder {
   constructor() {}
-  
+
+  // Execute the program and return the results as a file
   async run() {
     const filePath: string = await this.getFilePath();
     if (filePath) {
@@ -18,8 +19,10 @@ export class ArtistPairFinder {
       const tempPairs: Set<string> =
         this.assemblePotentialPairs(filteredArtists);
       const pairs = this.findRepeatingPairs(tempPairs, filteredArtists);
+      const fileExtensionIndex = filePath.indexOf('.');
+      const fileName = filePath.substring(0, fileExtensionIndex);
       writeFile(
-        path.join(__dirname, "Artist_lists_small_results.txt"),
+        path.join(__dirname, `${fileName}_results.csv`),
         pairs.join("\n"),
         (err) => {
           if (err) {
@@ -32,6 +35,7 @@ export class ArtistPairFinder {
     }
   }
 
+// Extracts the filepath from the command line input
   async getFilePath(): Promise<string> {
     const argv = await yargs.options({
       filePath: {
@@ -44,6 +48,8 @@ export class ArtistPairFinder {
     return argv.filePath;
   }
 
+  // Parses each line of the specified file.
+  // Then map each artist to a list of indexex indicating a list that contains the artist
   async parseFile(filePath: string): Promise<ArtistMap> {
     try {
       const fileStream: ReadStream = createReadStream(filePath);
@@ -70,6 +76,7 @@ export class ArtistPairFinder {
     }
   }
 
+  // Remove any artists that appear in less than 50 lists
   filterArtists(repeatingArtists: ArtistMap) {
     for (const artist in repeatingArtists) {
       if (repeatingArtists[artist].length < 50) {
@@ -79,6 +86,7 @@ export class ArtistPairFinder {
     return repeatingArtists;
   }
 
+  // Assemble a list of all potential pairs
   assemblePotentialPairs(repeatingArtists: ArtistMap): Set<string> {
     let tempPairs: Set<string> = new Set();
     for (let artist1 in repeatingArtists) {
@@ -102,6 +110,7 @@ export class ArtistPairFinder {
     return tempPairs;
   }
 
+  // Returns a list of pairs that appear together at least 50 times.
   findRepeatingPairs(
     tempPairs: Set<string>,
     repeatingArtists: ArtistMap
@@ -111,14 +120,14 @@ export class ArtistPairFinder {
       const artists = pair.split(",");
       const artist1 = artists[0];
       const artist2 = artists[1];
-      let timesAppeardTogether = 0;
+      let count = 0;
       for (const index in repeatingArtists[artist1]) {
         if (index in repeatingArtists[artist2]) {
-          timesAppeardTogether += 1;
+          count += 1;
         }
       }
 
-      if ((timesAppeardTogether = 50)) {
+      if ((count = 50)) {
         const pair = `${artist1},${artist2}`;
         finalPairs.push(pair);
         return;
